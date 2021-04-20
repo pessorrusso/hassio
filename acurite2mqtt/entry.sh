58b4c48c9e8cff5c6796150d113258d32770efe6
pessorrusso <pessorrusso+git@gmail.com>
2021-04-19 18:36:58 -0600

    Update entry.sh
    adding exit 1

--- acurite2mqtt/entry.sh
+++ acurite2mqtt/entry.sh
@@ -38,3 +38,5 @@ echo "EXPIREAFTER = " $EXPIREAFTER
 
 
 rtl_433  $PROTOCOL -C si  -F mqtt://$MQTT_HOST:$MQTT_PORT,user=$MQTT_USERNAME,pass=$MQTT_PASSWORD,retain=$MQTT_RETAIN,events=$MQTT_TOPIC/events,states=$MQTT_TOPIC/states,devices=$MQTT_TOPIC[/model][/id][/channel:0]  -M time -M protocol -M level | /scripts/rtl_433_mqtt_hass.py
+echo "Exiting..."
+exit 1
